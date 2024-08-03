48fd081ca16fa12c46cad683bb614382adc1777a
James Tansiri <tansirijames@gmail.com>
2024-08-03 07:33:30 +0700

    feat: add footer

---
+++ _data/contacts.js
@@ -0,0 +1,6 @@
+module.exports = {
+	instagram: "https://www.instagram.com/avivstudio_/",
+	facebook: "https://www.facebook.com/AVIVstudiobkk/",
+	email: "contact@avivstudio.co",
+	phones: ["+6688-888-8888", "+6688-888-8889"],
+};
